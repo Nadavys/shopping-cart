25afee9c50c07dda5c5446831b1ce844e50642ec
Nadav Somekh <nadav.somekh@Nadavs-MacBook-Pro.local>
2020-03-12 20:26:49 -0700

    Updates

---
+++ precache-manifest.7b597a9f881bcd6ab80661d928d96d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "291313829a1337173cfe",
+    "url": "/shopping-cart/static/css/main.4140e7dd.chunk.css"
+  },
+  {
+    "revision": "291313829a1337173cfe",
+    "url": "/shopping-cart/static/js/main.5893831c.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  },
+  {
+    "revision": "b2867ad441a6b4917ba2",
+    "url": "/shopping-cart/static/js/2.26bcecea.chunk.js"
+  },
+  {
+    "revision": "af5a423cb35bc68179f9ecad4914ccc8",
+    "url": "/shopping-cart/index.html"
+  }
+];
