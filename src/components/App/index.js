4217592f8cc3c37861bc240291c61bee3ed1f14e
Nadav Somekh <nadav.somekh@Nadavs-MacBook-Pro.local>
2020-03-24 18:36:07 -0700

    footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,14 +5,15 @@ import FloatCart from '../FloatCart';
 
 const App = () => (
   <React.Fragment>
-    <div className="main-header-title">
+    <div className="mainheader">
+      <div className="center">
       <h1>Bishulim SF 2020 Passover Order Menu</h1>
-      <h2>Contact us regarding inquries at <a href="mailto:bishulimsf@gmail.com">bishulimsf@gmail.com</a> <a href="tel:415-881-0843">415-881-0843</a> </h2>
-    </div>
+      <h2>Contact us for inquries at <a href="mailto:bishulimsf@gmail.com">bishulimsf@gmail.com</a> <a href="tel:415-881-0843">415-881-0843</a> </h2>
     <div className="sidebar-main">
 
       <ul>
-        <li>All kosher Ingredients (no kitniyot - legumes). The meal will be cooked at Temple Beth Abraham’s kosher for Passover kitchen.</li>
+        <li>All kosher Ingredients (no kitniyot - legumes). </li>
+          <li>The meal will be cooked at Temple Beth Abraham’s kosher for Passover kitchen.</li>
         <li>Portions are per person</li>
         <li>Delivery available for Bay Area locations. $25 per delivery.</li>
         <li>Delivery on April 8th</li>
@@ -25,10 +26,24 @@ const App = () => (
 
       </ul>
     </div>
+    </div>
+    </div>
     <main>
       <Shelf />
     </main>
     <FloatCart />
+    
+    
+<footer>
+  <div className="copyright">
+    <p>© 2020 - Bishulimsf</p>
+  </div>
+  <div className="social">
+    <a href="https://www.bishulimsf.com/" className="support" target="_blank" >Bishilimsf.com</a>
+    <a href="https://www.facebook.com/bishulimsf/" className="face" target="_blank">f</a>
+    <a href="https://www.instagram.com/bishulim_sf/" className="instagram" target="_blank">instagram</a>
+  </div>
+</footer>
   </React.Fragment>
 );
 
